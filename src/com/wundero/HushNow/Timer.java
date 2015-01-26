99634ce649c44ff3212cf6abf84c7c8c3c223a82
Wundero <sublimecraftftw@gmail.com>
2015-01-26 13:29:45 -0500

    Update Timer.java

--- src/com/wundero/HushNow/Timer.java
+++ src/com/wundero/HushNow/Timer.java
@@ -6,20 +6,18 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class Timer extends BukkitRunnable {
 
-	public int time = 120;
 	private Hush hush;
 	private Player player;
 	private CommandSender sender;
 	private boolean all;
 	private int ID;
 	
-	public Timer(Hush h, Player p, CommandSender s, boolean b, int t)
+	public Timer(Hush h, Player p, CommandSender s, boolean b)
 	{
 		this.hush = h;
 		this.player = p;
 		this.sender = s;
 		this.all = b;
-		this.time = t;
 	}
 	
 	public void setID(int i)
@@ -34,13 +32,7 @@ public class Timer extends BukkitRunnable {
 	
 	@Override
 	public void run() {
-		
-		if(time==0)
-		{
-			hush.stopTimer(player, sender, all, ID);
-		}
-		
-		time--;
+		hush.stopTimer(player, sender, all, ID);
 	}
 	
 
